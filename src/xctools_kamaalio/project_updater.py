21ee5f8bf654bfbffea61b41eb35af13ab7f400a
Kamaal Farah <kamaal.f1@gmail.com>
2024-04-28 14:13:47 +0200

    Updating multiple project files

--- src/xctools_kamaalio/project_updater.py
+++ src/xctools_kamaalio/project_updater.py
@@ -4,15 +4,20 @@ from kamaaalpy.dicts import omit_empty
 
 
 class ProjectUpdater:
-    project_configuration: Path
+    project_configurations: list[Path]
 
     def __init__(self) -> None:
+        project_configurations: list[Path] = []
         for path in Path.cwd().glob("**/*"):
-            if path.name == "project.pbxproj":
-                self.project_configuration = path
-                break
-        else:
+            if path.name != "project.pbxproj":
+                continue
+
+            project_configurations.append(path)
+
+        if len(project_configurations) == 0:
             raise ProjectUpdaterException("No project path found")
+
+        self.project_configurations = project_configurations
 
     def bump_version(self, build_number: int | None, version_number: str | None):
         has_changes = self.__edit(
@@ -28,13 +33,11 @@ class ProjectUpdater:
         else:
             print("No changes where needed")
 
-    def __edit(self, object_to_update: dict[str, str]):
-        if object_to_update == {}:
-            return False
-
-        project_configuration_file_lines = (
-            self.project_configuration.read_text().splitlines()
-        )
+    @staticmethod
+    def edit_configuration(
+        project_configuration: str, object_to_update: dict[str, str]
+    ):
+        project_configuration_file_lines = project_configuration.splitlines()
         keys_to_update = object_to_update.keys()
         has_changes = False
         for line_number, line in enumerate(project_configuration_file_lines):
@@ -44,21 +47,48 @@ class ProjectUpdater:
 
                 amount_of_tabs = line.count("\t")
                 tabs = "\t" * amount_of_tabs
-                project_configuration_file_lines[
-                    line_number
-                ] = f"{tabs}{key} = {object_to_update[key]};"
+                project_configuration_file_lines[line_number] = (
+                    f"{tabs}{key} = {object_to_update[key]};"
+                )
                 has_changes = True
                 break
 
         if not has_changes:
-            return False
+            return
 
         if len(project_configuration_file_lines[-1]) != 0:
             project_configuration_file_lines.append("")
 
-        self.project_configuration.write_text(
-            "\n".join(project_configuration_file_lines)
+        return "\n".join(project_configuration_file_lines)
+
+    def __edit(self, object_to_update: dict[str, str]):
+        if object_to_update == {}:
+            return False
+
+        has_some_changes = False
+        for project_configuration in self.project_configurations:
+            has_changes = ProjectUpdater.__edit_configuration(
+                project_configuration=project_configuration,
+                object_to_update=object_to_update,
+            )
+            if not has_changes:
+                continue
+            has_some_changes = True
+
+        return has_some_changes
+
+    @classmethod
+    def __edit_configuration(
+        cls, project_configuration: Path, object_to_update: dict[str, str]
+    ):
+        project_configuration_changes = cls.edit_configuration(
+            project_configuration=project_configuration.read_text(),
+            object_to_update=object_to_update,
         )
+        if project_configuration_changes is None:
+            return False
+
+        project_configuration.write_text(project_configuration_changes)
         return True
 
 
